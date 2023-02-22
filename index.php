51b03d4c1f50e351add93b0a4efa0f34af862445
leandro-pestana <leandrofap@gmail.com>
2023-02-21 22:10:09 -0300

    Add List

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 </head>
 <body>
     <h1>Teste instalando Git</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas ipsam eos labore eligendi dolores cupiditate dicta earum, maiores repellat. Enim amet ratione facilis ad quia sed delectus, vel voluptatibus dolores!</p>
+
+    <ul>
+        <li>list 01</li>
+    </ul>
 </body>
 </html>
